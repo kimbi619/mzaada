50faa65d3c07ed0ed5884fa39a127789de210636
kimbi619 <kimbidarl@gmail.com>
2024-08-07 04:52:37 -0400

    blog section complete

--- src/home/BlogSection.js
+++ src/home/BlogSection.js
@@ -9,6 +9,11 @@ const BlogSection = () => {
           <h3 className='blog_item_title'>Blog Title</h3>
           <img src='https://via.placeholder.com/300' alt='Blog' />
           <div className='blog_content'>
+            <div className='three_dots'>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
             <p className='blog_item_description'>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
               nec purus et libero.
@@ -20,6 +25,11 @@ const BlogSection = () => {
           <h3 className='blog_item_title'>Blog Title</h3>
           <img src='https://via.placeholder.com/300' alt='Blog' />
           <div className='blog_content'>
+            <div className='three_dots'>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
             <p className='blog_item_description'>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
               nec purus et libero.
